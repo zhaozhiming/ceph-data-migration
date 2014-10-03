df5d51eaf57645225839a9e7a4745a1134264043
zhaozhiming <kingzzm1982@sina.com>
2014-10-03 18:35:53 +0800

    add local jar files

---
+++ src/main/java/com/github/zzm/migration/main/Main.java
@@ -0,0 +1,4 @@
+package com.github.zzm.migration.main;
+
+public class Main {
+}
---
+++ src/test/java/com/github/zzm/migration/main/MainTest.java
@@ -0,0 +1,7 @@
+package com.github.zzm.migration.main;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+}
