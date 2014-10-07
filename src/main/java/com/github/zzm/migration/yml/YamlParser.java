6d3087f30f4b5f193656de23f1dd5876ce9539f9
zhaozhiming <kingzzm1982@sina.com>
2014-10-07 12:37:53 +0800

    don't put yml file to classpath

--- src/main/java/com/github/zzm/migration/yml/YamlParser.java
+++ src/main/java/com/github/zzm/migration/yml/YamlParser.java
@@ -4,8 +4,9 @@ import com.github.zzm.migration.model.Gateway;
 import com.github.zzm.migration.model.User;
 import org.ho.yaml.Yaml;
 
-import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.URL;
+import java.security.CodeSource;
 
 public class YamlParser {
     private static User user = parse("user.yml", User.class);
@@ -26,9 +27,11 @@ public class YamlParser {
 
     private static <T> T parse(String fileName, Class<T> iclass) {
         try {
-            InputStream inputStream = ClassLoader.getSystemResourceAsStream(fileName);
+            CodeSource src = YamlParser.class.getProtectionDomain().getCodeSource();
+            URL url = new URL(src.getLocation(), fileName);
+            InputStream inputStream = url.openStream();
             return Yaml.loadType(inputStream, iclass);
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(String.format("parse yaml file failed. file: %s.", fileName));
         }
     }
